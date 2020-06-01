ae60952fdf2a1ac287bb4a2630abd4cd85ff2f0f
Fuminori Hakoishi <fuminori.hakoishi@erii.co.jp>
2020-06-01 16:03:21 +0900

    windowsのコマンド実行できた

--- source/wifi.py
+++ source/wifi.py
@@ -1,5 +1,35 @@
-from pythonwifi.iwlibs import Wireless
+#! /usr/bin/env python
 
-wifi = Wireless('eth1')
-wifi.getEssid()
-wifi.getMode()
+import subprocess
+import os
+import sys
+
+
+def win_main():
+    print("win_main() start")
+
+    cmd = 'dir'
+
+    # os.system("dir")
+    try:
+        res = subprocess.run(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    except:
+        print("subprocess.check_output() failed")
+
+    print(res.stdout.decode('shift-jis'))
+
+    return
+
+
+def linux_main():
+    return
+
+
+if __name__ == '__main__':
+    print("start main")
+    print("your os = ", os.name)
+    if os.name == "nt":
+        win_main()
+    else:
+        linux_main()
