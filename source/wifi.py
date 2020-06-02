c5032a855ad88995900c57b58624afeb9d27cd7f
Fuminori Hakoishi <fuminori.hakoishi@erii.co.jp>
2020-06-02 17:16:21 +0900

    getUsingSSDIをコピペ 動作未確認

--- source/wifi.py
+++ source/wifi.py
@@ -3,6 +3,7 @@
 import subprocess
 import os
 import sys
+import re
 
 from win32wifi import Win32Wifi as ww
 
@@ -10,7 +11,7 @@ from win32wifi import Win32Wifi as ww
 def win_main():
     print("win_main() start")
 
-    cmd = 'netsh wlan show'
+    cmd = 'netsh wlan show networks mode=Bssid'
 
     # os.system("dir")
     try:
@@ -31,27 +32,27 @@ def linux_main():
 def main():
     interfaces = ww.getWirelessInterfaces()
     print("WLAN Interfaces: {:d}".format(len(interfaces)))
+
     handle = ww.WlanOpenHandle()
     for idx, interface in enumerate(interfaces):
-        print("\n  {:d}\n  GUID: [{:s}]\n  Description: [{:s}]".format(
-            idx, interface.guid_string, interface.description))
+        print("\n  {:d}\n  GUID: [{:s}]\n  Description:  [{:s}]\n  State: [{:s}]".format(
+            idx, interface.guid_string, interface.description, interface.state_string))
+
         try:
             scan_result = ww.WlanScan(handle, interface.guid)
+
         except:
             print(sys.exc_info())
             continue
+
         print("\n  Scan result: {:d}".format(scan_result))
-    ww.WlanCloseHandle(handle)
 
+#   fafafa
+        print("scan end")
 
-def smain():
-    interfaces = ww.getWirelessInterfaces()
-    print("WLAN Interfaces: {:d}".format(len(interfaces)))
-    for idx0, interface in enumerate(interfaces):
-        print("\n  {:d}\n  GUID: [{:s}]\n  Description:  [{:s}]\n  State: [{:s}]".format(
-            idx0, interface.guid_string, interface.description, interface.state_string))
         try:
             networks = ww.getWirelessAvailableNetworkList(interface)
+
         except:
             print(sys.exc_info())
             continue
@@ -61,6 +62,35 @@ def smain():
         for idx1, network in enumerate(networks):
             print("\n    {:d}\n    SSID: [{:s}]\n    Profile: [{:}]\n    Connectable: {:}\n    Signal quality: {:d}\n    Flags: {:d}\n    Security: {:}\n    Auth: {:}".format(
                 idx1, network.ssid.decode(), network.profile_name, network.connectable, network.signal_quality, network.flags, network.security_enabled, network.auth))
+
+    ww.WlanCloseHandle(handle)
+
+
+class WiFiUtil:
+    # Wi-Fi関連ユーティリティ
+
+    @staticmethod
+    def getUsingSSID():
+        # 現在接続中のWi-FiのSSIDを取得する
+        # returns:
+        #   str -- SSID
+
+        # wifiの状況を確認するコマンドを実行する
+        cmd = ['/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', '-I']
+        cmd_res = subprocess.run(cmd, stdout=subprocess.PIPE)
+
+        # コマンドの実行結果をoutputに保存する
+        output = cmd_res.stdout.decode('utf-8')
+
+        # 文字列から、SSIDと書かれた部分を取得する
+        matchs = re.findall(r' +SSID: .+', output)
+        res = ""
+
+        if len(matchs) > 0:
+            res = matchs[0]
+            res = re.sub(r'^ +SSID: ', '', res)
+            res = res.rstrip()
+        return res
 
 
 if __name__ == '__main__':
@@ -73,10 +103,11 @@ if __name__ == '__main__':
         main()
         print("\nDone.")
 
-        print("Python {0:s} {1:d}bit on {2:s}\n".format(" ".join(item.strip(
-        ) for item in sys.version.split("\n")), 64 if sys.maxsize > 0x100000000 else 32, sys.platform))
-        smain(*sys.argv[1:])
-        print("\nDone.")
+        win_main()
+
+        # 現在のWiFiのSSIDを取得する
+        # res = WiFiUtil.getUsingSSID()
+        # print(res)
 
     else:
         linux_main()
