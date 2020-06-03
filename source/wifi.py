e23c43f54ca3e15863cb97ddbfc13e632a79568c
Fuminori Hakoishi <fuminori.hakoishi@erii.co.jp>
2020-06-03 13:00:42 +0900

    mainをclass化した

--- source/wifi.py
+++ source/wifi.py
@@ -8,66 +8,66 @@ import re
 from win32wifi import Win32Wifi as ww
 
 
-def win_main():
-    print("win_main() start")
-
-    cmd = 'netsh wlan show networks mode=Bssid'
-
-    # os.system("dir")
-    try:
-        res = subprocess.run(
-            cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-    except:
-        print("subprocess.check_output() failed")
-
-    print(res.stdout.decode('shift-jis'))
-
-    return
-
-
-def linux_main():
-    return
-
-
-def main():
-    interfaces = ww.getWirelessInterfaces()
-    print("WLAN Interfaces: {:d}".format(len(interfaces)))
-
-    handle = ww.WlanOpenHandle()
-    for idx, interface in enumerate(interfaces):
-        print("\n  {:d}\n  GUID: [{:s}]\n  Description:  [{:s}]\n  State: [{:s}]".format(
-            idx, interface.guid_string, interface.description, interface.state_string))
-
-        try:
-            scan_result = ww.WlanScan(handle, interface.guid)
-
-        except:
-            print(sys.exc_info())
-            continue
-
-        print("\n  Scan result: {:d}".format(scan_result))
-
-#   fafafa
-        print("scan end")
-
-        try:
-            networks = ww.getWirelessAvailableNetworkList(interface)
-
-        except:
-            print(sys.exc_info())
-            continue
-
-        print("\n  Networks: {:d}".format(len(networks)))
-
-        for idx1, network in enumerate(networks):
-            print("\n    {:d}\n    SSID: [{:s}]\n    Profile: [{:}]\n    Connectable: {:}\n    Signal quality: {:d}\n    Flags: {:d}\n    Security: {:}\n    Auth: {:}".format(
-                idx1, network.ssid.decode(), network.profile_name, network.connectable, network.signal_quality, network.flags, network.security_enabled, network.auth))
-
-    ww.WlanCloseHandle(handle)
-
-
 class WiFiUtil:
     # Wi-Fi関連ユーティリティ
+
+    @staticmethod
+    def win_main():
+        print("win_main() start")
+
+        cmd = 'netsh wlan show networks mode=Bssid'
+
+        # os.system("dir")
+        try:
+            res = subprocess.run(
+                cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        except:
+            print("subprocess.check_output() failed")
+
+        print(res.stdout.decode('shift-jis'))
+
+        return
+
+    @staticmethod
+    def linux_main():
+        return
+
+    @staticmethod
+    def main():
+        interfaces = ww.getWirelessInterfaces()
+        print("WLAN Interfaces: {:d}".format(len(interfaces)))
+
+        handle = ww.WlanOpenHandle()
+        for idx, interface in enumerate(interfaces):
+            print("\n  {:d}\n  GUID: [{:s}]\n  Description:  [{:s}]\n  State: [{:s}]".format(
+                idx, interface.guid_string, interface.description, interface.state_string))
+
+            try:
+                scan_result = ww.WlanScan(handle, interface.guid)
+
+            except:
+                print(sys.exc_info())
+                continue
+
+            print("\n  Scan result: {:d}".format(scan_result))
+
+    #   fafafa
+            print("scan end")
+
+            try:
+                networks = ww.getWirelessAvailableNetworkList(interface)
+
+            except:
+                print(sys.exc_info())
+                continue
+
+            print("\n  Networks: {:d}".format(len(networks)))
+
+            for idx1, network in enumerate(networks):
+                print("\n    {:d}\n    SSID: [{:s}]\n    Profile: [{:}]\n    Connectable: {:}\n    Signal quality: {:d}\n    Flags: {:d}\n    Security: {:}\n    Auth: {:}".format(
+                    idx1, network.ssid.decode(), network.profile_name, network.connectable, network.signal_quality, network.flags, network.security_enabled, network.auth))
+
+        ww.WlanCloseHandle(handle)
 
     @staticmethod
     def getUsingSSID():
@@ -100,7 +100,7 @@ if __name__ == '__main__':
         # win_main()
         print("Python {0:s} {1:d}bit on {2:s}\n".format(" ".join(item.strip(
         ) for item in sys.version.split("\n")), 64 if sys.maxsize > 0x100000000 else 32, sys.platform))
-        main()
+        WiFiUtil.main()
         print("\nDone.")
 
         win_main()
